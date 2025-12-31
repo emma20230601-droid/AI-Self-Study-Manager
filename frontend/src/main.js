f420b97bf922f058231068eacce2969072c78c59
emma20230601-droid <emma20230601@gmail.com>
2025-12-31 16:50:01 +0800

    Update main.js
    修正：Uncaught ReferenceError: axios is not defined

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
+import axios from 'axios';
 
 const app = createApp(App)
 app.use(router)
